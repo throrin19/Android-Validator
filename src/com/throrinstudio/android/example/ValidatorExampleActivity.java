e698b9193caf44bd68d17fbb146baae6db51efd0
throrin19 <ffbpta6d>
2012-06-26 13:35:03 +0200

    Init Project

---
+++ src/com/throrinstudio/android/example/ValidatorExampleActivity.java
@@ -0,0 +1,15 @@
+package com.throrinstudio.android.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ValidatorExampleActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
