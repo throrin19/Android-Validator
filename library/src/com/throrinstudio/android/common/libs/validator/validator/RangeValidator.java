098fbb382b63ae9f7166ce8171aebb9787f3c813
Exikle <dixondcunha@hotmail.com>
2014-06-06 02:10:32 -0400

    Signed-off-by: Exikle <dixondcunha@hotmail.com>

--- library/src/com/throrinstudio/android/common/libs/validator/validator/RangeValidator.java
+++ library/src/com/throrinstudio/android/common/libs/validator/validator/RangeValidator.java
@@ -4,17 +4,36 @@ import android.content.Context;
 
 import com.throrinstudio.android.common.libs.validator.AbstractValidator;
 
+/**
+ * Validates whether the given value is bettween a range of values
+ * 
+ * @author Exikle
+ */
 public class RangeValidator extends AbstractValidator {
 
 	/**
-	 * The range's start
+	 * The start of the range
 	 */
 	final double START_RANGE;
 
+	/**
+	 * The end of the range
+	 */
 	final double END_RANGE;
 
+	/**
+	 * The error Id from the string resource
+	 */
 	private static int mErrorMessage; // Your custom error message
 
+	/**
+	 * @param context
+	 * @param start
+	 *            of the range
+	 * @param end
+	 *            of the range
+	 * @param errorMessageRes
+	 */
 	public RangeValidator(Context c, double start, double end,
 			int errorMessageRes) {
 		super(c, errorMessageRes);
