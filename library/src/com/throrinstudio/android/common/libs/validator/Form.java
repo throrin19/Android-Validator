4c47d416ca49e47432e33bcc4db0bc6db594c66e
Exikle <dixondcunha@hotmail.com>
2014-06-06 16:56:59 -0400

    Added removeValidates to the form, which also returns it
    Signed-off-by: Exikle <dixondcunha@hotmail.com>

--- library/src/com/throrinstudio/android/common/libs/validator/Form.java
+++ library/src/com/throrinstudio/android/common/libs/validator/Form.java
@@ -7,35 +7,49 @@ import java.util.List;
  * Form Validation Class
  * <p/>
  * Immediately, only works with EditText
- *
+ * 
  * @author throrin19
  * @version 1.0
  */
 public class Form {
 
-    private List<AbstractValidate> mValidates = new ArrayList<AbstractValidate>();
+	private List<AbstractValidate> mValidates = new ArrayList<AbstractValidate>();
 
-    /**
-     * Function adding Validates to our form
-     *
-     * @param validate {@link AbstractValidate} Validate to add
-     */
-    public void addValidates(AbstractValidate validate) {
-        mValidates.add(validate);
-    }
+	/**
+	 * Function adding Validates to our form
+	 * 
+	 * @param validate
+	 *            {@link AbstractValidate} Validate to add
+	 */
+	public void addValidates(AbstractValidate validate) {
+		mValidates.add(validate);
+	}
 
-    /**
-     * Called to validate our form.
-     * If an error is found, it will be displayed in the corresponding field.
-     *
-     * @return boolean true if the form is valid, otherwise false
-     */
-    public boolean validate() {
-        boolean formValid = true;
-        for (AbstractValidate validate : mValidates) {
-            //  Use & in order to evaluate both side of the operation.
-            formValid = formValid & validate.isValid();
-        }
-        return formValid;
-    }
+	/**
+	 * Function removing Validates from our form
+	 * 
+	 * @param validate
+	 *            {@link AbstractValidate} Validate to remove
+	 * @return validate that was removed from the form
+	 */
+	public boolean removeValidates(AbstractValidate validate) {
+		if (mValidates != null && !mValidates.isEmpty()) {
+			return mValidates.remove(validate);
+		}
+		return false;
+	}
+
+	/**
+	 * Called to validate our form.
+	 * If an error is found, it will be displayed in the corresponding field.
+	 * 
+	 * @return boolean true if the form is valid, otherwise false
+	 */
+	public boolean validate() {
+		boolean formValid = true;
+		for (AbstractValidate validate : mValidates) {
+			formValid = formValid & validate.isValid();	// Use & in order to evaluate both side of the operation.
+		}
+		return formValid;
+	}
 }
