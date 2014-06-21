7ac275f144126271fb94e0dde644fb01233dcece
Exikle <dixondcunha@hotmail.com>
2014-06-20 20:52:17 -0400

    Added documentation for IP validator
    Signed-off-by: Exikle <dixondcunha@hotmail.com>

--- library/src/com/throrinstudio/android/common/libs/validator/validator/IPAddressValidator.java
+++ library/src/com/throrinstudio/android/common/libs/validator/validator/IPAddressValidator.java
@@ -8,16 +8,30 @@ import android.content.Context;
 import com.throrinstudio.android.common.libs.validator.AbstractValidator;
 import com.throrinstudio.android.common.libs.validator.R;
 
+/**
+ * Validates whether ip digits is between 0-255 digits, has only numbers and is the correct format
+ * 
+ * @author Dixon D'Cunha (Exikle)
+ */
 public class IPAddressValidator extends AbstractValidator {
 
+	/**
+	 * IP Addresses format
+	 */
 	private static final String IPADDRESS_STRING_PATTERN = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
 			+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
 			+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
 			+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
 
-	private static final Pattern IPADDRESS_PATTERN = Pattern
+	/**
+	 * Compiled pattern for the format
+	 */
+	private final Pattern IPADDRESS_PATTERN = Pattern
 			.compile(IPADDRESS_STRING_PATTERN);
 
+	/**
+	 * Default message if none specified
+	 */
 	private static final int DEFAULT_ERROR_MESSAGE_RESOURCE = R.string.validator_ip;
 
 	public IPAddressValidator(Context c) {
@@ -26,7 +40,6 @@ public class IPAddressValidator extends AbstractValidator {
 
 	@Override
 	public boolean isValid(String ip) {
-		Matcher matcher = IPADDRESS_PATTERN.matcher(ip);
-		return matcher.matches();
+		return IPADDRESS_PATTERN.matcher(ip).matches();
 	}
 }
