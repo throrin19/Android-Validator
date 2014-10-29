249cbcba34dbd0e3c7a8c7c19e77c3872247deaf
Justin Pollard <jp@zumper.com>
2014-10-29 11:25:15 -0700

    Added ability to specify custom error/invalid behavior through CustomErrorNotification

--- library/src/com/throrinstudio/android/common/libs/validator/Validate.java
+++ library/src/com/throrinstudio/android/common/libs/validator/Validate.java
@@ -6,13 +6,21 @@ import java.util.List;
 import android.graphics.drawable.Drawable;
 import android.widget.TextView;
 
+import com.throrinstudio.android.common.libs.validator.interfaces.CustomErrorNotification;
+
 public class Validate extends AbstractValidate {
 
     private List<AbstractValidator> mValidators = new ArrayList<AbstractValidator>();
     private TextView mSourceView;
+    private CustomErrorNotification mCustomErrorNotification;
 
     public Validate(TextView sourceView) {
         mSourceView = sourceView;
+    }
+
+    public Validate(TextView sourceView, CustomErrorNotification customErrorNotification) {
+        mSourceView = sourceView;
+        mCustomErrorNotification = customErrorNotification;
     }
 
     /**
@@ -28,12 +36,22 @@ public class Validate extends AbstractValidate {
         for (AbstractValidator validator : mValidators) {
             try {
                 if (!validator.isValid(mSourceView.getText().toString())) {
-                    setSourceViewError(validator.getMessage(), validator.getErrorDrawable());
+                    if(mCustomErrorNotification != null) {
+                        mCustomErrorNotification.onInvalid(this);
+                    } else {
+                        setSourceViewError(validator.getMessage(), validator.getErrorDrawable());
+                    }
                     return false;
+                } else {
+                  if(mCustomErrorNotification != null) mCustomErrorNotification.onValid(this);
                 }
             } catch (ValidatorException e) {
                 e.printStackTrace();
-                setSourceViewError(e.getMessage(), validator.getErrorDrawable());
+                if(mCustomErrorNotification != null) {
+                    mCustomErrorNotification.onInvalid(this);
+                } else {
+                    setSourceViewError(e.getMessage(), validator.getErrorDrawable());
+                }
                 return false;
             }
         }
@@ -45,17 +63,21 @@ public class Validate extends AbstractValidate {
         return mSourceView;
     }
 
-  /**
-   * Sets error on {@link #mSourceView}.
-   *
-   * @param errorMessage  String : the error message
-   * @param errorDrawable Drawable : the drawable to display
-   */
-    private void setSourceViewError(String errorMessage, Drawable errorDrawable) {
-        if(errorDrawable != null) {
-          mSourceView.setError(errorMessage, errorDrawable);
-        } else {
-          mSourceView.setError(errorMessage);
-        }
+    public List<AbstractValidator> getValidators() {
+        return mValidators;
     }
+
+    /**
+     * Sets error on {@link #mSourceView}.
+     *
+     * @param errorMessage  String : the error message
+     * @param errorDrawable Drawable : the drawable to display
+     */
+      private void setSourceViewError(String errorMessage, Drawable errorDrawable) {
+          if(errorDrawable != null) {
+            mSourceView.setError(errorMessage, errorDrawable);
+          } else {
+            mSourceView.setError(errorMessage);
+          }
+      }
 }
